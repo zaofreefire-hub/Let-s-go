e53aaa2150856f2ba95b984f6edfbbe58dd44ad0
zaofreefire-hub <zaofreefire@gmail.com>
2025-12-29 13:46:08 +0100

    Add Cloudstream plugin configuration

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("com.lagradost.cloudstream3.gradle-plugin")
+}
+
+cloudstream {
+    language.set("ar")
+    description.set("First success. Just proof.")
+    authors.set(listOf("Edigiya Store"))
+}
