c20ff0cdbc17328da2e43540b0ae71cafb47ea77
zaofreefire-hub <zaofreefire@gmail.com>
2025-12-29 14:30:20 +0100

    Update CloudStream plugin version and metadata

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    id("com.lagradost.cloudstream3.gradle-plugin")
+    id("com.lagradost.cloudstream3.gradle-plugin") version "1.1.0"
 }
 
 cloudstream {
     language.set("ar")
-    description.set("First success. Just proof.")
-    authors.set(listOf("Edigiya Store"))
+    description.set("First working CloudStream provider")
+    authors.set(listOf("zaofreefire-hub"))
 }
