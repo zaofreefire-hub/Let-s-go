2d3f56727cfabac0bb81ed2816cecfd23c4bb5cb
zaofreefire-hub <zaofreefire@gmail.com>
2025-12-29 14:34:52 +0100

    Add Kotlin plugin and update dependencies
    Updated build configuration to include Kotlin and additional repositories.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,17 @@
 plugins {
-    id("com.lagradost.cloudstream3.gradle-plugin") version "1.1.0"
+    kotlin("jvm") version "1.9.22"
 }
 
-cloudstream {
-    language.set("ar")
-    description.set("First working CloudStream provider")
-    authors.set(listOf("zaofreefire-hub"))
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    implementation("com.github.recloudstream:cloudstream:4.4.0")
+}
+
+tasks.register<Jar>("buildCs3") {
+    archiveExtension.set("cs3")
+    from(sourceSets.main.get().output)
 }
